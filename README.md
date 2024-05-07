433d9580c10c4189e2968b2c9315b0ac05028582
ikuo <ikuo.pg@gmail.com>
2024-05-07 12:54:21 +0000

    debug

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lifegame-js
 life game for javascript
+
+# WEBサーバ起動
+
+python -m http.server 8000
+python -m http.server 8000 --directory ./contents
--- index.js
+++ index.js
@@ -7,6 +7,65 @@ var KEY_DOWN  = 0x0008;
 var KEY_A     = 0x0100;
 var KEY_B     = 0x0200;
 
+function LinearCongruentialGenerator(seed) {
+    var m = 4294967296; // 2^32
+    var a = 1664525;
+    var c = 1013904223;
+    var state = seed;
+    this.next = function() {
+        state = (a * state + c) % m;
+        return state / m;
+    }
+}
+
+function Random() {
+	var self = this;
+	self.generator = new LinearCongruentialGenerator(0);
+	self.seed = function(n) {
+		var me = this;
+		me.generator = new LinearCongruentialGenerator(n);
+	}
+	self.rand = function() {
+		var me = this;
+		return me.generator.next();
+	}
+	self.intRange = function(min, max) {
+		var me = this;
+		return Math.floor(me.generator.next() * (max - min + 1)) + min;
+	}
+}
+var random = new Random();
+
+function LifeGame() {
+    var self = this;
+    self.width = 64;
+    self.height = 64;
+    self.setupMatrix = function() {
+        var me = this;
+        var matrix = n2d.create(me.width, me.height);
+        for(var y = 0; y < me.height; y += 1) {
+            for(var x = 0; x < me.width; x += 1) {
+                matrix[y][x] = Math.random() < 0.4 ? 1 : 0;
+            }
+        }
+    }
+    self.applyRulesToMatrix = function(matrix) {
+        var me = this;
+        var newMatrix = n2d.clone(matrix);
+        var ruleBirth = [0, function(a, d){return a == 3;}];
+        var ruleAlive = [1, function(a, d){return a == 2 || a == 3;}];
+        var ruleDead1 = [1, function(a, d){return a <= 1;}];
+        var ruleDead2 = [1, function(a, d){return a >= 4;}];
+        for(var y = 0; y < me.height; y += 1) {
+            for(var x = 0; x < me.width; x += 1) {
+                var deadOrAlive = me.checkRule(me.height, me.width, y, x, matrix, ruleBirth);
+                newMatrix[y][x] = deadOrAlive;
+            }
+        }
+        return newMatrix;
+    }
+}
+
 function GameMain() {
     var self = this;
     
@@ -16,23 +75,23 @@ function GameMain() {
         return img;
     };
     
-    self.playerMove = function(k) {
+    self.playerMove = function(gi) {
         var r2 = 0.707106781188095;//1 / 1.41421356237;
-        if(k & KEY_LEFT && k & KEY_UP) {
+        if(gi.left && gi.up) {
             return [-r2, -r2];
-        } else if(k & KEY_LEFT && k & KEY_DOWN) {
+        } else if(gi.left && gi.down) {
             return [-r2, r2];
-        } else if(k & KEY_RIGHT && k & KEY_UP) {
+        } else if(gi.right && gi.up) {
             return [r2, -r2];
-        } else if(k & KEY_RIGHT && k & KEY_DOWN) {
+        } else if(gi.right && gi.down) {
             return [r2, r2];
-        } else if(k & KEY_LEFT) {
+        } else if(gi.left) {
             return [-1.0, 0.0];
-        } else if(k & KEY_RIGHT) {
+        } else if(gi.right) {
             return [1.0, 0.0];
-        } else if(k & KEY_UP) {
+        } else if(gi.up) {
             return [0.0, -1.0];
-        } else if(k & KEY_DOWN) {
+        } else if(gi.down) {
             return [0.0, 1.0];
         }
         return [0.0, 0.0];
@@ -90,77 +149,94 @@ function Draw(ctx, x) {
     }
 }
 
+function GameInput() {
+    var self = this;
+    self.left = 0;
+    self.right = 0;
+    self.up = 0;
+    self.down = 0;
+    self.a = 0;
+    self.b = 0;
+    self.c = 0;
+}
+
 function MainLoop() {
     var self = this;
-    self.keys = new Array(256);
+    self.keyCodes = [];
+    self.keyEvents = [];
     
     addEventListener("keydown", function(e) {
-        self.keys[e.keyCode] = 1;
+        self.keyCodes.push(e.code);
+        self.keyEvents.push(1);
         e.preventDefault();
     }, false);
     
     addEventListener("keyup", function(e) {
-        self.keys[e.keyCode] = 0;
+        self.keyCodes.push(e.code);
+        self.keyEvents.push(0);
     }, false);
     
-    self.deviceToInput = function() {
-        var me = this;
-        var k = me.keys;
-        var x = 0;
-        if(k[37] || k[65]) {
-            x |= KEY_LEFT;
-        }
-        if(k[38] || k[87]) {
-            x |= KEY_UP;
-        }
-        if(k[39] || k[68]) {
-            x |= KEY_RIGHT;
-        }
-        if(k[40] || k[83]) {
-            x |= KEY_DOWN;
-        }
-        if(k[32] || k[0]) {
-            x |= KEY_A;
-        }
-        if(k[13] || k[0]) {
-            x |= KEY_B;
-        }
-        return x;
-    }
-    
-    var g = new GameMain();
-    var t = performance.now();
-    var fpst = t;
-    var framen = 0;
-    var framerec = 0;
-    var mainLoop = function(timestamp) {
-        if((timestamp - fpst) > 1000) {
-            fpst = performance.now();
-            framerec = framen;
-            framen = 0;
-        }
-        while(1) {
-            if((performance.now() - t) > 16.66) {
-                t = performance.now();
-                var input = self.deviceToInput();
-                g.main(input);
-                
-                var canvas = document.getElementById("main_canvas");
-                var ctx = canvas.getContext("2d");
-                Draw(ctx, g.getDraw());
-                
-                ctx.font = '20px sans-serif';
-                ctx.fillText(String(framerec) + "fps", 0, 32);
-                framen += 1;
-                break;
+    self.deviceToInput = function(gameInput, codes, events) {
+        codes.forEach(function(code, i) {
+            if(["ArrowLeft", "KeyA"].includes(code)) {
+                gameInput.left = events[i];
+                return;
             }
-        }
-        requestAnimationFrame(mainLoop);
+            if(["ArrowRight", "KeyD"].includes(code)) {
+                gameInput.right = events[i];
+                return;
+            }
+            if(["ArrowUp", "KeyW"].includes(code)) {
+                gameInput.up = events[i];
+                return;
+            }
+            if(["ArrowDown", "KeyS"].includes(code)) {
+                gameInput.down = events[i];
+                return;
+            }
+        });
     }
-    mainLoop();
+
+    self.MainProcess = function() {
+        var g = new GameMain();
+        var t = performance.now();
+        var fpst = t;
+        var framen = 0;
+        var framerec = 0;
+        var gameInput = new GameInput();
+        var updatelimit = 0;
+        var mainLoop = function() {
+            var timestamp = performance.now();
+            if((timestamp - fpst) > 1000) {
+                fpst = timestamp;
+                framerec = framen;
+                framen = 0;
+            }
+            if(timestamp >= updatelimit) {
+                updatelimit = timestamp + 15;
+                self.deviceToInput(gameInput, self.keyCodes, self.keyEvents);
+                self.keyCodes = [];
+                self.keyEvents = [];
+                g.main(gameInput);
+                framen += 1;
+            }
+            setTimeout(mainLoop);
+        }
+        var drawLoop = function() {
+            var canvas = document.getElementById("main_canvas");
+            var ctx = canvas.getContext("2d");
+            Draw(ctx, g.getDraw());
+            ctx.font = '20px sans-serif';
+            ctx.fillText(String(framerec) + "fps", 0, 32);
+            requestAnimationFrame(drawLoop);
+        }
+        mainLoop();
+        drawLoop();
+    }
 }
 
 
 window.onload = function() {
     var g = new MainLoop();
+    g.MainProcess();
 }
